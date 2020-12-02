75ed4d385a3785f69711232d7a12121b63932c17
YMYGABE <51685770+YMYGABE@users.noreply.github.com>
2020-12-02 15:46:42 +0800

    添加唱片旋转和进度条

---
+++ app/src/main/java/com/example/music/MyIntentService.java
@@ -0,0 +1,25 @@
+package com.example.music;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MyIntentService extends IntentService {
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
